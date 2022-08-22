05ed9d54e7b72e503ae0f5d9301deb515a9b0e7f
Roman92P <roman.paszkow@gmail.com>
2022-08-22 23:30:02 +0200

    Initial commit

---
+++ Anti-Fraud System/task/src/antifraud/app/model/Amount.java
@@ -0,0 +1,19 @@
+package antifraud.app.model;
+
+import java.io.Serializable;
+
+/**
+ * Roman Pashkov created on 22.08.2022 inside the package - antifraud.app.model
+ */
+public class Amount{
+
+    private long amount;
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(long amount) {
+        this.amount = amount;
+    }
+}
